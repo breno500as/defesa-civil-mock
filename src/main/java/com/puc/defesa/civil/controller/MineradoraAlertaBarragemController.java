6a10a2742d5e40c0e4f3ae41b0630b1b287012e2
breno500as <breno500as@gmail.com>
2020-04-29 22:40:57 -0300

    adequação de api

--- src/main/java/com/puc/defesa/civil/controller/MineradoraAlertaBarragemController.java
+++ src/main/java/com/puc/defesa/civil/controller/MineradoraAlertaBarragemController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("mineradora-alerta-barragem")
+@RequestMapping("alertas-barragem")
 public class MineradoraAlertaBarragemController {
 
 	@PostMapping(consumes = "application/json")
